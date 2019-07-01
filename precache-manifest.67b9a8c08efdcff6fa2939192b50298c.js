747911195806b5743ee6fde8002cb9d113210e4b
test <test>
2019-07-01 08:33:57 -0400

    Updates

---
+++ precache-manifest.67b9a8c08efdcff6fa2939192b50298c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91d9d50619efbd9170dd1be7a7b45c0",
+    "url": "/redux-basics/index.html"
+  },
+  {
+    "revision": "550e7834722afe6a26a2",
+    "url": "/redux-basics/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a81032b70d0255fafd0f",
+    "url": "/redux-basics/static/js/2.750d86f1.chunk.js"
+  },
+  {
+    "revision": "550e7834722afe6a26a2",
+    "url": "/redux-basics/static/js/main.5ebdae83.chunk.js"
+  },
+  {
+    "revision": "d716271bcc3575fcb454",
+    "url": "/redux-basics/static/js/runtime~main.84cf328c.js"
+  }
+]);
